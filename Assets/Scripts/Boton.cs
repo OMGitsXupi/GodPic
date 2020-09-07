0f5825e28c3c5a9392abd3eee5a250ca3689bd56
xupi <antoniogs9@hotmail.es>
2020-09-07 18:20:51 +0200

    juego completo sin menu

---
+++ Assets/Scripts/Boton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour
+{
+    public GameObject activador;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(new Vector3(0f, 1f, 0f), ForceMode.Impulse);
+    }
+
+    private void OnTriggerEnter(Collider objeto)
+    {
+        if (objeto.gameObject == activador)
+            GetComponent<AudioSource>().Play();
+    }
+
+    private void OnCollisionEnter(Collision objeto)
+    {
+        if (objeto.gameObject == activador)
+            GetComponent<AudioSource>().Play();
+    }
+}
